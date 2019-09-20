80614a8913f789d10b91921cd76bceb748b1dfa4
suvarna.purohit@gmail.com <suvarna_purohit@yahoo.com>
2019-09-20 15:14:58 +0530

    Added code to show the values entered by user in the text boxes

--- JS_Files/SampleJS.js
+++ JS_Files/SampleJS.js
@@ -5,7 +5,7 @@ btn.style.background = 'red';
 console.log(btn);
 
 const name = document.querySelector('#name');
-name.value = "Suvarna";
+name.value = "<Change My Name>";
 
 const title = document.querySelector('h1');
 title.innerText = "Testing Javascript";
@@ -13,4 +13,6 @@ title.innerText = "Testing Javascript";
 btn.addEventListener('click', e=> {
     e.preventDefault();
     title.innerText = "Testing Javascript Event Listner";
+    document.getElementById('name_entered').innerText = document.getElementById('name').value;
+    document.getElementById('email_entered').innerText = document.getElementById('email').value;
 })
